b5b43fc38e67412b4e94653c00c379b87bde54ef
Tanapon65030 <s6503052412030@email.kmutnb.ac.th>
2023-12-04 11:33:55 +0700

    add document

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 console.log("Hello")
-<<<<<<< HEAD
 console.log("Hello GitHub")
 console.log("Hello Tanapon")
-=======
 console.log("GitHub")
 console.log("Tanapon")
->>>>>>> 82308ece7808b983261ff4f6c65fe0b5f98b3603
